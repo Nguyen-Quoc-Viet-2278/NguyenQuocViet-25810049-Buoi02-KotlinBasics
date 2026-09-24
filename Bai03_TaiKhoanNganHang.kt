ba8ade21e4d47c3da1de0b718c9503315b30964c
Nguyen Quoc Viet <email_github_cua_ban@gmail.com>
2026-09-24 11:08:33 +0700

    Buoi02 Bai03 tai khoan ngan hang

---
+++ Bai03_TaiKhoanNganHang.kt
@@ -0,0 +1,10 @@
+// Nguyen Quoc Viet - 25810049
+fun main () {
+    val soDuBanDau: Int = 5000000
+    var soDuHienTai: Int = soDuBanDau
+   	soDuHienTai = soDuHienTai + 2000000
+    println("Số dư hiện tại $soDuHienTai đồng" )
+    soDuHienTai = soDuHienTai - 1480000
+    println("Số tiền đã rút ra $soDuHienTai đồng")
+    
+}

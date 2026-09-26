679d77215d5ec90e7b44f0f61401d92409ab8e89
Nguyen Quoc Viet <email_github_cua_ban@gmail.com>
2026-09-26 19:40:19 +0700

    Buoi02 Bai02 gioi thieu ban than

---
+++ Buoi02/Bai02_GioiThieuBanThan.kt
@@ -0,0 +1,9 @@
+// Nguyen Quoc Viet - 25810049
+fun main () {
+    val hoTen: String = "Nguyen Quoc viet"
+    val namSinh = 2004
+    val namHientai = 2026
+    val tuoi = namHientai - namSinh
+    
+    println(" Tôi ten là $hoTen, nay nam tôi $tuoi tuổi ") 
+}
